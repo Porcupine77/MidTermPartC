c7b7ccda3a2ee2d7441de3cbb6a031937adf485a
Amanpreet Rai <rai34@shernet.sheridancollege.ca>
2024-02-21 14:50:58 -0500

    Sol4 commit

---
+++ src/midtermexam_versiona_extensioncode/amanpreet.java
@@ -0,0 +1,12 @@
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author amanp
+ */
+public class amanpreet {
+    
+    // My name is Amanpreet Rai
+    // Today is February 21, 2024
+
+}
